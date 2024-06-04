a0d03bc7dba0bacf78d78222167d3527f9b7d8a5
FemRene <prorene@outlook.de>
2024-06-04 17:53:55 +0200

    initial Commit

--- src/main/java/de/femrene/Main.java
+++ src/main/java/de/femrene/Main.java
@@ -1,17 +1,47 @@
 package de.femrene;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    private static JDABuilder builder;
+    private static JDA jda;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Usage: java -jar MusicBot.jar <token>");
+            System.exit(1);
+        }
+        System.out.println("[BOT] Starting Bot");
+        builder = JDABuilder.createDefault(args[0]);
+        builder.setAutoReconnect(true);
+        builder.setStatus(OnlineStatus.ONLINE);
+        builder.setActivity(Activity.customStatus("Playing your favourite Music"));
+        builder.enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_VOICE_STATES);
+        jda = builder.build();
+        System.out.println("[BOT] Ready");
+        stop();
+    }
+
+    private static void stop() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            if (reader.readLine().equalsIgnoreCase("stop")) {
+                System.out.println("[BOT] Shutting down...");
+                builder.setStatus(OnlineStatus.OFFLINE);
+                jda.shutdown();
+                reader.close();
+                System.out.println("[BOT] Stopped");
+                System.exit(0);
+            }
         }
     }
 }
--- src/main/java/de/femrene/lavaplayer/PlayerManager.java
+++ src/main/java/de/femrene/lavaplayer/PlayerManager.java
@@ -1,2 +1,86 @@
-package de.femrene.lavaplayer;public class PlayerManager {
+package de.femrene.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlayerManager {
+
+    private static PlayerManager INSTANCE;
+    private Map<Long, GuildMusicManager> guildMusicManagers = new HashMap<>();
+    private AudioPlayerManager audioPlayerManager = new DefaultAudioPlayerManager();
+
+    private PlayerManager() {
+        AudioSourceManagers.registerRemoteSources(audioPlayerManager);
+        AudioSourceManagers.registerLocalSource(audioPlayerManager);
+    }
+
+    public static PlayerManager get() {
+        if (INSTANCE == null) {
+            INSTANCE = new PlayerManager();
+        }
+        return INSTANCE;
+    }
+
+    public GuildMusicManager getGuildMusicManager(Guild guild) {
+        return guildMusicManagers.computeIfAbsent(guild.getIdLong(), (guildid) -> {
+            GuildMusicManager guildMusicManager = new GuildMusicManager(audioPlayerManager);
+
+            guild.getAudioManager().setSendingHandler(guildMusicManager.getAudioForwarder());
+            return guildMusicManager;
+        });
+    }
+
+    public void play(Guild guild, String trackURL) {
+        GuildMusicManager guildMusicManager = getGuildMusicManager(guild);
+        audioPlayerManager.loadItemOrdered(guildMusicManager, trackURL, new AudioLoadResultHandler() {
+
+            /**
+             * Called when the requested item is a track and it was successfully loaded.
+             *
+             * @param track The loaded track
+             */
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                guildMusicManager.getTrackScheduler().queue(track);
+            }
+
+            /**
+             * Called when the requested item is a playlist and it was successfully loaded.
+             *
+             * @param playlist The loaded playlist
+             */
+            @Override
+            public void playlistLoaded(AudioPlaylist playlist) {
+
+            }
+
+            /**
+             * Called when there were no items found by the specified identifier.
+             */
+            @Override
+            public void noMatches() {
+
+            }
+
+            /**
+             * Called when loading an item failed with an exception.
+             *
+             * @param exception The exception that was thrown
+             */
+            @Override
+            public void loadFailed(FriendlyException exception) {
+
+            }
+        });
+    }
+
 }
